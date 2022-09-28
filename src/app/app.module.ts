9e6569c2b722d32ecb0f608fe1743bbccd54d9a8
JohnABardo <johnabardo01@gmail.com>
2022-09-28 18:15:43 -0400

    Added payment component, routing directive, got rid of unecessary import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CatalogComponent } from './catalog/catalog.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { PaymentInfoComponent } from './payment-info/payment-info.component';
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     AppComponent,
-    CatalogComponent
+    CatalogComponent,
+    PaymentInfoComponent
   ],
   imports: [
     BrowserModule,
@@ -17,6 +20,9 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     BrowserAnimationsModule,
     MaterialModule,
     FlexLayoutModule,
+    RouterModule.forRoot([
+      {path: 'payment-info', component: PaymentInfoComponent},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cart } from '../data/models/cart';
 import { LineItem } from '../data/models/line-item';
-import { temp_items } from './temp_catalog_items'; //contains the items for display
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.component.html',
