0a28f18decc87ffeda51b63f9a0ed075522f213c
unknown <malikclarke10@gmail.com>
2022-10-01 22:13:42 -0400

    bug fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CatalogComponent } from './catalog/catalog.component';
 import { ShippingComponent } from './shipping/shipping.component';
+import { CartComponent } from './cart/cart.component';
 const routes: Routes = [
   {path: 'catalog', component: CatalogComponent},
-  {path: 'shipping', component: ShippingComponent}
+  {path: 'shipping', component: ShippingComponent},
+  {path: 'cart', component: CartComponent}
 ];
 
 @NgModule({
